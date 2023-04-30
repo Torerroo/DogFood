551dbba97b7534bc6c53a7c5a216221399bec516
Torerroo <Torerro1996@mail.ru>
2023-04-30 12:51:00 +0300

    update Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,27 +1,15 @@
 import './Main.css'
-import courierDog from '../../images/courier-dogs.png'
 
 export function Main() {
   return (
     <main className="main">
-      <h1>Магазин DogFood</h1>
-      <p>
-        {/* Только у нас вы сможете встретить
+      <h1>
+        Добро пожаловать
+        <br />
+        в интернет-магазин
         {' '}
-        <em>таких</em>
-        {' '}
-        курьеров */}
-        Оформите заказ и наши курьеры доставят ваш заказ в этот же день.
-      </p>
-      <img src={courierDog} alt="courierDog" />
-      <p>
-        Так же проходит
-        {' '}
-        <b>акция!</b>
-        {' '}
-        Если вы понравитесь нашим курьерам,
-        то они могут оставить вам бонусный товар.
-      </p>
+        <span>DogFood</span>
+      </h1>
     </main>
   )
 }
